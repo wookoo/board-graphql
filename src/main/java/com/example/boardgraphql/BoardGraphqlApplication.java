a71264bfec01ab86e9fd910817f68c9fbf60fab9
김도현 <nanayagoon@daum.net>
2024-09-09 16:01:10 +0900

    Init: 프로젝트 초기화

---
+++ src/main/java/com/example/boardgraphql/BoardGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardGraphqlApplication.class, args);
+    }
+
+}
